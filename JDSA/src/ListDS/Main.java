f7dfaa58f0d3658df33b2ce469d822744b62f250
Gopal Mishra <gopal_mishra@apple.com>
2020-08-20 13:44:54 +0530

    List

---
+++ JDSA/src/ListDS/Main.java
@@ -0,0 +1,14 @@
+package ListDS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Employee> employeeList = new ArrayList<>();
+		
+	}
+
+}
