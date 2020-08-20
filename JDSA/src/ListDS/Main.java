af1c9f15e89d595793e5262844dd66650103fd5c
gmrokz <gpl31.msh@gmail.com>
2020-08-20 17:21:22 +0530

    Linked list

--- JDSA/src/ListDS/Main.java
+++ JDSA/src/ListDS/Main.java
@@ -9,6 +9,36 @@ public class Main {
 		
 		List<Employee> employeeList = new ArrayList<>();
 		
+		employeeList.add(new Employee("jane","jones",12));
+		employeeList.add(new Employee("jhon","Doe",123));
+		employeeList.add(new Employee("gopal","mishra",324));
+		
+/*		employeeList.forEach(employee ->System.out.println(employee));
+		System.out.println(employeeList.get(1));
+		System.out.println(employeeList.isEmpty());*/
+		employeeList.set(1, new Employee("jhon","adams",4568));
+//		System.out.println(employeeList.size());
+		
+		employeeList.add(3, new Employee("jhon","doe",4567));
+//		employeeList.forEach(employee ->System.out.println(employee));
+		
+		Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+		
+		for(Employee employee:employeeArray) {
+			
+			System.out.println(employee);
+			
+		}
+		
+		/*System.out.println(employeeList.contains(new Employee("gopal","mishra",324)));
+		
+		System.out.println(employeeList.indexOf(new Employee("gopal","mishra",324)));*/
+		
+		employeeList.remove(3);
+		
+		employeeList.forEach(employee ->System.out.println(employee));
+
+		
 	}
 
 }
