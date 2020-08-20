6be8aa9dd45440d3eaf8e9d5000d2b6a1d532449
gmrokz <gpl31.msh@gmail.com>
2020-08-21 01:04:58 +0530

    Stack Examples

---
+++ JDSA/src/Stacks/StackWithArray.java
@@ -0,0 +1,22 @@
+package Stacks;
+
+public class StackWithArray {
+
+	public static void main(String[] args) {
+
+		ArrayStack stack = new ArrayStack(10);
+		
+		stack.push(new Employee("Jane","Jones",123));
+		stack.push(new Employee("John","Doe",4567));
+		stack.push(new Employee("Marry","Smith",22));
+		stack.push(new Employee("Mike","Wilson",3245));
+		stack.push(new Employee("Bill","End",78));
+		
+		stack.printStack();
+		System.out.println(stack.peek());
+		//stack.printStack();
+		
+		stack.pop();
+	}
+
+}
