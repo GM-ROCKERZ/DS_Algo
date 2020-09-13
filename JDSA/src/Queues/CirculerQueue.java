ea4021b5b3bf9b8b9d0f98ceded32a58ce950db9
gmrokz <gpl31.msh@gmail.com>
2020-09-13 22:37:28 +0530

    my recent commit

--- JDSA/src/Queues/CirculerQueue.java
+++ JDSA/src/Queues/CirculerQueue.java
@@ -17,27 +17,21 @@ public class CirculerQueue {
 		
 		if(size() == queue.length-1) {
 			
-			int numOfItems = size();
 			Employee2[] newArray = new Employee2[2*queue.length]; 
 			
-		
-			System.arraycopy(queue, front, newArray, 0, queue.length-front);
-			System.arraycopy(queue, 0, newArray, queue.length-front, back);
+			System.arraycopy(queue, 0, newArray, 0, queue.length);
 			
 			queue = newArray;
-			
-			front = 0;
-			back =numOfItems;
 		}
 		
 		queue[back] = employee;
-		if(back < queue.length-1) {
-			back++;
+		
+		if(back<queue.length-1) {
+		back++;
 		}
 		else {
-			back=0;
+			back =0;
 		}
-		
 	}
 	
 	public Employee2 remove() {
@@ -52,10 +46,7 @@ public class CirculerQueue {
 			front =0;
 			back = 0;
 		}
-		else if(front == queue.length){
-
-			front=0;
-		}
+		
 		return employee;
 		
 	}
@@ -69,34 +60,17 @@ public class CirculerQueue {
 		return queue[front];
 	}
 	public int size() {
-	
-		if(front<=back) {
-			return back-front;
-		}
-		else {
-			return back-front+queue.length;
-		}
+		return back-front;
 	}
 	
 	public void printQueue() {
-		if(front<=back) {
 		
 		for(int i = front;i<back;i++) {
 			System.out.println(queue[i]);
 		}
 	}
 	
-	else {
-		
-		for(int i =front;i<queue.length;i++) {
-			System.out.println(queue[i]);
-		}
-		
-		for(int i =0;i < back;i++) {
-			System.out.println(queue[i]);
-		}
-	}
 	
-	}
+	
 
 }
---
+++ JDSA/src/Stacks/GitPrac.java
@@ -0,0 +1,9 @@
+package Stacks;
+
+public class GitPrac {
+
+	public static void main(String[] args) {
+
+	}
+
+}
